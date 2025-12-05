ec7e912403d914c8c0abcc56a207d800bf305d6b
leebock <lbock@esri.com>
2025-12-05 14:20:28 -0500

    moved config json into config.js

---
+++ src/config.js
@@ -0,0 +1,47 @@
+/**
+ * Configuration for Census ACS (American Community Survey) services and field mappings
+ */
+export const CENSUS_CONFIG = {
+  "ACS Population and Housing Basics": {
+    tract: {
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_10_14_Highlights_Population_Housing_Basics_Boundaries/FeatureServer/2',
+      fields: {
+        name: 'NAME',
+        state: 'State',
+        county: 'County',
+        medianContractRent: 'B25058_001E',
+        medianHomeValue: 'B25077_001E',
+        medianIncome: 'B19049_001E'
+      }
+    },
+    state: {
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_10_14_Highlights_Population_Housing_Basics_Boundaries/FeatureServer/0',
+      fields: {
+        name: 'NAME',
+        medianContractRent: 'B25058_001E',
+        medianHomeValue: 'B25077_001E',
+        medianIncome: 'B19049_001E'
+      }
+    }
+  },
+  "ACS Housing Costs": {
+    tract: {
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Housing_Costs_Boundaries/FeatureServer/2',
+      fields: {
+        name: 'NAME',
+        state: 'State',
+        county: 'County',
+        medianContractRent: 'B25058_001E',
+        medianHomeValue: 'B25077_001E'
+      }
+    },
+    state: {
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Housing_Costs_Boundaries/FeatureServer/0',
+      fields: {
+        name: 'NAME',
+        medianContractRent: 'B25058_001E',
+        medianHomeValue: 'B25077_001E'
+      }
+    }
+  }
+};
--- src/main.js
+++ src/main.js
@@ -2,51 +2,7 @@ import './style.css'
 import { parseLatLonFromURL } from './coordinates.js'
 import { fetchTractByLatLon } from './censusApi.js'
 import { displayTractInfo, displayNoDataMessage, displayErrorMessage } from './ui.js'
-
-const CENSUS_CONFIG = {
-  "ACS Population and Housing Basics": {
-    tract: {
-      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_10_14_Highlights_Population_Housing_Basics_Boundaries/FeatureServer/2',
-      fields: {
-        name: 'NAME',
-        state: 'State',
-        county: 'County',
-        medianContractRent: 'B25058_001E',
-        medianHomeValue: 'B25077_001E',
-        medianIncome: 'B19049_001E'
-      }
-    },
-    state: {
-      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_10_14_Highlights_Population_Housing_Basics_Boundaries/FeatureServer/0',
-      fields: {
-        name: 'NAME',
-        medianContractRent: 'B25058_001E',
-        medianHomeValue: 'B25077_001E',
-        medianIncome: 'B19049_001E'
-      }
-    }
-  },
-  "ACS Housing Costs": {
-    tract: {
-      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Housing_Costs_Boundaries/FeatureServer/2',
-      fields: {
-        name: 'NAME',
-        state: 'State',
-        county: 'County',
-        medianContractRent: 'B25058_001E',
-        medianHomeValue: 'B25077_001E'
-      }
-    },
-    state: {
-      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/ACS_Housing_Costs_Boundaries/FeatureServer/0',
-      fields: {
-        name: 'NAME',
-        medianContractRent: 'B25058_001E',
-        medianHomeValue: 'B25077_001E'
-      }
-    }
-  }
-};
+import { CENSUS_CONFIG } from './config.js'
 
 async function main() {
 
