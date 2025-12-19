cd31949ebb3c49e1ca29efa0e7eddaef074a851f
leebock <lbock@esri.com>
2025-12-19 15:52:42 -0500

    adding linting stuff

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        $arcgis: "readonly"
+      }
+    }
+  }
+];
